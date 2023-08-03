629daff43b82301eeac37bdcf3d8921dbce8cd67
Rafaela Souza <Rafaela.Santos-ext@ab-Inbev.com>
2023-08-03 16:26:07 -0300

    Adicionando destaques

--- group3-coderefine/out/extension.js
+++ group3-coderefine/out/extension.js
@@ -6,7 +6,7 @@ const axios_1 = require("axios");
 const endpoint = "https://api.openai.com/v1/chat/completions";
 const headers = {
     "Content-Type": "application/json",
-    Authorization: "Bearer ",
+    Authorization: "Bearer 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",
 };
 const model = "gpt-3.5-turbo";
 const temperature = 0.6;
@@ -15,6 +15,15 @@ function activate(context) {
         const text = e.getText();
         execute(text);
     });
+    const revisionDecorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: 'rgba(255, 0, 0, 0.2)',
+        border: '1px solid red' // Border for the highlight
+    });
+    let disposable2 = vscode.commands.registerTextEditorCommand('extension.highlightPhrases', (editor) => {
+        highlightPhrases(editor, revisionDecorationType);
+        executeForHighlight(editor);
+    });
+    context.subscriptions.push(revisionDecorationType, disposable2);
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
@@ -31,6 +40,38 @@ async function getSuggestion(prompt) {
         }, { headers: headers });
         vscode.window.showInformationMessage("Resposta do ChatGPT: " +
             JSON.stringify(response.data.choices[0].message.content));
+    }
+    catch (error) {
+        vscode.window.showErrorMessage("Erro ao enviar a mensagem para o ChatGPT.");
+        console.error(error);
+    }
+}
+function highlightPhrases(editor, revisionDecorationType) {
+    const text = editor.document.getText();
+    // Define regular expression patterns to match phrases
+    const revisionPattern = /\"\<\<.*?\>\>/g;
+    const decorations = [];
+    let match;
+    while ((match = revisionPattern.exec(text)) !== null) {
+        const startPos = editor.document.positionAt(match.index);
+        const endPos = editor.document.positionAt(match.index + match[0].length);
+        const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Needs revision' };
+        decorations.push(decoration);
+    }
+    // Apply decorations to the editor
+    editor.setDecorations(revisionDecorationType, decorations);
+}
+async function executeForHighlight(editor) {
+    const prompt = "Atue como um desenvolvedor de software e gere sugestões de melhorias para o código abaixo.\n\n" + editor.document.getText();
+    getSuggestionForHighlights(prompt);
+}
+async function getSuggestionForHighlights(prompt) {
+    try {
+        const response = await axios_1.default.post(endpoint, {
+            model: model,
+            messages: [{ role: "user", content: prompt }],
+            temperature: temperature,
+        }, { headers: headers });
     }
     catch (error) {
         vscode.window.showErrorMessage("Erro ao enviar a mensagem para o ChatGPT.");
--- group3-coderefine/src/extension.ts
+++ group3-coderefine/src/extension.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const endpoint = "https://api.openai.com/v1/chat/completions";
 const headers = {
 	"Content-Type": "application/json",
-	Authorization: "Bearer ",
+	Authorization: "Bearer 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",
 };
 const model = "gpt-3.5-turbo";
 const temperature = 0.6;
@@ -12,10 +12,22 @@ const temperature = 0.6;
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.workspace.onDidSaveTextDocument((e) => {
 		const text = e.getText();
-	
 		execute(text);
 	});
 
+	const revisionDecorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: 'rgba(255, 0, 0, 0.2)', // Highlight color
+        border: '1px solid red' // Border for the highlight
+    });
+
+    let disposable2 = vscode.commands.registerTextEditorCommand(
+        'extension.highlightPhrases', (editor) => {
+            highlightPhrases(editor, revisionDecorationType);
+			executeForHighlight(editor);
+        }		
+    );
+
+    context.subscriptions.push(revisionDecorationType, disposable2);
 	context.subscriptions.push(disposable);
 }
 
@@ -40,6 +52,51 @@ async function getSuggestion(prompt: string) {
 			"Resposta do ChatGPT: " +
 			JSON.stringify(response.data.choices[0].message.content)
 		);
+	} catch (error) {
+		vscode.window.showErrorMessage(
+			"Erro ao enviar a mensagem para o ChatGPT."
+		);
+		console.error(error);
+	}
+}
+
+function highlightPhrases(editor: vscode.TextEditor, revisionDecorationType: vscode.TextEditorDecorationType) {
+    const text = editor.document.getText();
+    // Define regular expression patterns to match phrases
+    const revisionPattern = /\"\<\<.*?\>\>/g;
+
+    const decorations: vscode.DecorationOptions[] = [];
+    
+    let match;
+    while ((match = revisionPattern.exec(text)) !== null) {
+        const startPos = editor.document.positionAt(match.index);
+        const endPos = editor.document.positionAt(match.index + match[0].length);
+
+        const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Needs revision' };
+        decorations.push(decoration);
+    }
+
+    // Apply decorations to the editor
+    editor.setDecorations(revisionDecorationType, decorations);
+}
+
+async function executeForHighlight(editor: vscode.TextEditor)  { 
+	const prompt = "Atue como um desenvolvedor de software e gere sugestões de melhorias para o código abaixo.\n\n" + editor.document.getText();
+	getSuggestionForHighlights(prompt);
+}
+
+async function getSuggestionForHighlights(prompt: string) {
+	try {
+		const response = await axios.post(
+			endpoint,
+			{
+				model: model,
+				messages: [{ role: "user", content: prompt }],
+				temperature: temperature,
+			},
+			{ headers: headers }
+		);
+		
 	} catch (error) {
 		vscode.window.showErrorMessage(
 			"Erro ao enviar a mensagem para o ChatGPT."
